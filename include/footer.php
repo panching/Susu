2ebeb39b7b241f8750c718813bbbab78e9bc24e7
Eric Pan <panching825@gmail.com>
2018-02-24 22:49:12 +0800

    release

---
+++ include/footer.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="container text-left">
+        <div class="row">
+            <div class="widget col-xs-12 col-sm-3 small_font">
+                <p>住所 :<br/>東京都千代田区一ツ橋2-6-2 <br class='hidden-xs'/>日本教育会館 B1</p>
+                <p>営業時間 :<br/>11〜16時（日月祝休,金土は22時迄）<br/>※本の閲覧は平日ランチタイム以外でお願いします</p>
+                <p>電話 :<br/>非公開<br/>※予約は貸切/素貸のみ可能。連絡はメールでお願いします。</p><p>mail :<br/><A HREF="mailto:miraishokudo@gmail.com">miraishokudo@gmail.com</a></p>
+            </div>
+            <div class="widget col-xs-12 col-sm-4 small_font">
+                <p>アクセス :<br/>
+                半蔵門線神保町駅（A1出口）徒歩3分<br/>
+                三田線神保町駅（A1出口）徒歩5分<br/>
+                東西線竹橋駅（1b出口）徒歩5分<br/>
+                東西線九段下駅（6番出口）徒歩7分</p>
+                <a href="http://www.jec.or.jp/koutuu/" target='_blank'>日本教育会館様 交通案内地図</a>
+                <br/><br/>
+                <a href='http://www.facebook.com/miraisyokudo/' target='_blank'><i class="fa fa-facebook-square fa-2x footer_icon"></i></a>&nbsp;&nbsp;
+                <a href='https://github.com/miraishokudo/home_page' target='_blank'><i class="fa fa-github-square fa-2x footer_icon"></i></a> </div>
+            <div class="widget col-xs-12 col-sm-5 map">
+            <a href='https://www.google.co.jp/maps/place/%E6%97%A5%E6%9C%AC%E6%95%99%E8%82%B2%E4%BC%9A%E9%A4%A8/@35.694342,139.756234,17z/data=!3m1!4b1!4m2!3m1!1s0x60188c13b8af0a3f:0x9c26ea740eb79b4d' target="_blank">
+                <img src="../img/access_map.png" width="100%">
+            </a>
+            <div class='visible-xs' style="height: 20px;"></div>
+            </div>
+        </div>
+    </div>
+</footer>
