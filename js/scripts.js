360c86a0083d1b074a7e346d11cafb3abf873d2a
LAPTOP-PPOO0CTI\User <choupan089@gmail.com>
2018-05-31 21:48:00 +0900

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,11 @@ $(document).ready(function() {
   }
 
   function centering_news() {
-		var PageHight = window.innerHeight;
 		var w = $( window ).width() ;
 		var h = $( window ).height() ;
 		var cw = $("#news").outerWidth();
 		var ch = $("#news").outerHeight();
 		var top = ( $( window ).width() > 768 ) ? 133 : 60;
-		$("#news").css( {"left": ((w - cw)/2) + "px","top": top + "px","height": (PageHight/1.5) + "px"} ) ;
+		$("#news").css( {"left": ((w - cw)/2) + "px","top": top + "px"} ) ;
 	}	
 });
